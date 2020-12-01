6383c0aab8cc9e8943c0674e6ba45a69c9e611e1
nikinov <novellenicholas@gmail.com>
2020-12-01 19:21:13 +0100

    creating project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // move forward constantly add here
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * speed);
+        }
+        else if(Input.GetKeyDown(KeyCode.K))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
